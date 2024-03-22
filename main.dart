e8fc7e6b8a1f96ddd3c6fe75dfc36f450a7e5ecd
Alexey Inkin <leha@inkin.ru>
2024-03-22 11:52:19 +0400

    Add dummy workflows

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-import 'package:googleapis_auth/auth_io.dart';
-
-Future<void> main() async {
-  print(File(Platform.environment['GOOGLE_APPLICATION_CREDENTIALS']!).readAsStringSync());
-  await clientViaApplicationDefaultCredentials(scopes: []);
-}
