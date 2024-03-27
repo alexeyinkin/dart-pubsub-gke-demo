eacea30b10638770b8847e9e4d427bdb26fde08a
Alexey Inkin <leha@inkin.ru>
2024-03-27 12:04:29 +0400

    Clean up

--- capitalizer/test/main_test.dart
+++ capitalizer/test/main_test.dart
@@ -4,9 +4,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:gcloud/pubsub.dart';
-import 'package:googleapis_auth/auth_io.dart';
-import 'package:http/http.dart' as http;
-// import 'package:process/process.dart';
 import 'package:test/test.dart';
 import 'package:wif_workaround/wif_workaround.dart' as w;
 
@@ -26,8 +23,6 @@ void main() {
         .readAsStringSync());
     final pubsub = PubSub(
       await w.clientViaApplicationDefaultCredentials(scopes: PubSub.SCOPES),
-      //await clientViaApplicationDefaultCredentials(scopes: PubSub.SCOPES),
-      // await _getClient(),
       projectId,
     );
 
@@ -64,46 +59,3 @@ void main() {
     expect(actual, expected);
   });
 }
-
-// Future<http.Client> _getClient() async {
-//   try {
-//     return await clientViaApplicationDefaultCredentials(scopes: PubSub.SCOPES);
-//     // return await clientViaMetadataServer();
-//     //} on ServerRequestFailedException catch (ex) {
-//   } catch (ex) {
-//     print(ex.toString());
-//     // print(ex.responseContent);
-//     return await _getWorkloadIdentityFederationClient();
-//     // rethrow;
-//   }
-// }
-//
-// Future<http.Client> _getWorkloadIdentityFederationClient() async {
-//   final process = await Process.run('gcloud', ['auth', 'print-access-token']);
-//
-//   if (process.exitCode != 0) {
-//     throw Exception(
-//       'Command failed with exit code ${process.exitCode}: ${process.stderr}',
-//     );
-//   }
-//
-//   final accessToken = process.stdout as String;
-//   print(accessToken.length);
-//   print(accessToken.substring(0, 5));
-//   print(accessToken.substring(accessToken.length - 5));
-//
-//   final accessTokenTrimmed = accessToken.trimRight();
-//
-//   var authClient = authenticatedClient(
-//     http.Client(),
-//     AccessCredentials(
-//       AccessToken('Bearer', accessTokenTrimmed, DateTime.now().add(Duration(hours: 1)).toUtc()),
-//       null, // Refresh token is null because we are manually setting the access token
-//       //['https://www.googleapis.com/auth/cloud-platform'], // Scopes
-//       PubSub.SCOPES,
-//     ),
-//   );
-//
-//   print(authClient);
-//   return authClient;
-// }
